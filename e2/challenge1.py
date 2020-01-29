bad2a145d4386aef78ef67d5f4ce5a14993c39dc
Billy <billy>
2020-01-29 20:01:49 +0000

    - typo fix

--- e2/challenge1.py
+++ e2/challenge1.py
@@ -1,4 +1,4 @@
-# Challenge 1 : Load the JSON string and print the value of "job"
+# Challenge 1 : Load the JSON string and return the value of "job"
 # jsonstring = '{"name":"Billy", "age":25, "city":"London", "job":"being awesome"}'
 class challenge1:
     def run(self, jsonstring):
