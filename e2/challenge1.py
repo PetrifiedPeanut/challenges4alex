cf72b85a577c4e35c3214ff8d687bad4d803cc3f
PetrifiedPeanut <60445688+PetrifiedPeanut@users.noreply.github.com>
2020-01-29 20:56:46 +0000

    Added for loop

--- e2/challenge1.py
+++ e2/challenge1.py
@@ -1,12 +1,13 @@
 # Challenge 1 : Load the JSON string and return the value of "job"
 # jsonstring = '{"name":"Billy", "age":25, "city":"London", "job":"being awesome"}'
+import json
 class challenge1:
     def run(self, jsonstring):
         #your code here
         solution = jsonstring
         return solution
 
-
-
-
-
+for items in challenge1:
+    y = json.loads(x)
+    print(json.dumps({"name"}))
+    print(y)
